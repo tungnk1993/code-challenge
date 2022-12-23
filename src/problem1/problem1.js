69ee75990558cacb33f03ef36b5402d4b62e223c
nkt <nkt@nkts-MacBook-Air.local>
2022-12-23 15:14:21 +0700

    problem1

--- src/problem1/problem1.js
+++ src/problem1/problem1.js
@@ -1,11 +1,19 @@
 var sum_to_n_a = function(n) {
-    // your code here
+    return (n*(n+1))/2;
 };
 
 var sum_to_n_b = function(n) {
-    // your code here
+    let s = 0;
+    for (let i = 1; i <= n; i++) {
+        s += i;
+    }
+    return s;
 };
 
 var sum_to_n_c = function(n) {
-    // your code here
+    if (n == 0) {
+        return n;
+    } else {
+        return n + sum_to_n_c(n-1);
+    }
 };
