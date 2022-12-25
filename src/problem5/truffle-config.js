83ae67b3e1bb9b4185bb518702f725cd83783dc0
nkt <nkt@nkts-MacBook-Air.local>
2022-12-25 21:09:14 +0700

    wip problem5

---
+++ src/problem5/truffle-config.js
@@ -0,0 +1,40 @@
+const { projectId, mnemonic } = require('./secret.env');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+module.exports = {
+  networks: {
+    development: {
+     host: "127.0.0.1",     // Localhost (default: none)
+     port: 8545,            // Standard Ethereum port (default: none)
+     network_id: "*",       // Any network (default: none)
+    },
+     goerli: {
+      provider: () => new HDWalletProvider(mnemonic, `https://goerli.infura.io/v3/${projectId}`),
+      network_id: 5,       
+      gas: 5500000,        
+      confirmations: 2,    
+      timeoutBlocks: 200,  
+      skipDryRun: true   
+    },
+  },
+
+  // Set default mocha options here, use special reporters, etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.8.17",      // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
+      //  evmVersion: "byzantium"
+      // }
+    }
+  },
+};
